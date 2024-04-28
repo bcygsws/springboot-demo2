0fb3e3c1f14c6fefe04f7fea3483543fc7f3dab6
bcygsws <bcygswspring168@gmail.com>
2024-04-28 23:35:29 +0800

    :sparkles: 注解@ModelAttribute

--- src/main/java/com/example/springbootdemo2/demos/web/BasicController.java
+++ src/main/java/com/example/springbootdemo2/demos/web/BasicController.java
@@ -58,10 +58,24 @@ public class BasicController {
 		return "index.html";
 	}
 
+	/*
+	 * Model是模型，表示把数据放在返回的页面上
+	 * @RequestParam(name = "name", defaultValue = "unknown user") String name,实际上是一个赋值的过程
+	 * 注解@RequestParam将请求参数中的name、age求解出来，并分别赋给Stirng和Integer类型的name和age变量
+	 *
+	 *
+	 * */
 	@ModelAttribute
 	public void parseUser(@RequestParam(name = "name", defaultValue = "unknown user") String name
 			, @RequestParam(name = "age", defaultValue = "12") Integer age, User user) {
+		/*以下代码，在处理没有传入name和age时，例如访问：http://127.0.0.1:8080/save_user，index.html页面中就会显示：user will save: name=zhangsan, age=18*/
 		user.setName("zhangsan");
 		user.setAge(18);
 	}
 }
+// 注解@ModelAttribute
+//   参考文档：https://blog.csdn.net/u010502101/article/details/78876295?utm_medium=distribute.pc_relevant.none-task-blog-2~default~baidujs_baidulandingword~default-4-78876295-blog-105740360.235^v43^control&spm=1001.2101.3001.4242.3&utm_relevant_index=7
+//
+//   执行时机；
+//   运行http://127.0.0.1:8080/save_user后，控制视图方法saveUser响应该请求，但是在执行该方法之前会先执行@ModelAttribute标注的parseUser()方法
+
